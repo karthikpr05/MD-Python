771ff9597891609bbe4612feaee3ffdd1a2dbc27
Karthik P.R <karthikpr05@gmail.com>
2026-07-11 09:23:19 +0530

    funtions topic

---
+++ function.py
@@ -0,0 +1,25 @@
+# def greet():
+#     print("Welcome to python")
+# greet()
+
+
+# def display():
+#     print("Hello")
+#     print("good morning")
+# display()
+
+# def greet(name):
+#     print("Hello" , name)
+# greet("Karthik")
+
+
+def student(name , age):
+    print(name)
+    print(age)
+
+student("Karthik",35)
+student("Rajani",34)
+student("Sushrut",7)
+student("Raghuchandra", 70)
+student("Chitra",68)
+
